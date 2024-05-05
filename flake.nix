d7c37628bab9561fd6496bc4565b9557f7a4a6b9
Peter Crona <petercrona89@gmail.com>
2024-05-05 21:19:08 +0200

    format and update desc

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example Nix Flake for Haskell";
+  description = "IceColdCode Dev Environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
@@ -10,14 +10,16 @@
       system = "x86_64-linux";
       ghcVersion = "ghc98";
       pkgs = import nixpkgs { system = system; };
-      
+
       example-haskell-nix-flake = pkgs.haskell.packages.${ghcVersion}.developPackage {
         root = ./.;
         returnShellEnv = true;
-        modifier = drv: pkgs.haskell.lib.addBuildTools drv [ pkgs.haskell.packages.${ghcVersion}.cabal-install
-                                                             pkgs.haskell.packages.${ghcVersion}.haskell-language-server
-                                                             pkgs.awscli2
-                                                           ];
+        modifier = drv: pkgs.haskell.lib.addBuildTools drv [
+          pkgs.haskell.packages.${ghcVersion}.cabal-install
+          pkgs.haskell.packages.${ghcVersion}.haskell-language-server
+          pkgs.awscli2
+          pkgs.nixpkgs-fmt
+        ];
       };
     in
     {
